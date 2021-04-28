8b35811904cb11fdd5025f8640f7791fe7be7b37
mzvast <mzvast@gmail.com>
2021-04-28 16:41:45 +0800

    add-two-numbers

---
+++ top/add-two-numbers/addTwoNumbers.2.js
@@ -0,0 +1,77 @@
+// 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+// 请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+// 输入：l1 = [2,4,3], l2 = [5,6,4]
+// 输出：[7,0,8]
+// 解释：342 + 465 = 807.
+// 示例 2：
+
+// 输入：l1 = [0], l2 = [0]
+// 输出：[0]
+// 示例 3：
+
+// 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// 输出：[8,9,9,9,0,0,0,1]
+
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+}
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    let leftCur = l1,
+        rightCur = l2,
+        cur = null,
+        head = null;
+    let carry = 0; // 进位
+    while (leftCur || rightCur || carry) {
+        const leftVal = leftCur ? leftCur.val : 0;
+        const rightVal = rightCur ? rightCur.val : 0;
+        sum = leftVal + rightVal + carry;
+        const newNode = new ListNode(sum % 10);
+        carry = Math.floor(sum / 10);
+        if (!cur) {
+            head = cur = newNode;
+        } else {
+            cur.next = newNode;
+            cur = newNode;
+        }
+        leftCur && (leftCur = leftCur.next);
+        rightCur && (rightCur = rightCur.next);
+    }
+    return head;
+};
+
+// 从左到右是低位到高位，加法要考虑一个进位的问题,用一个flag来保存进位。
+// [2,4,3]
+// [5,6,4]
+// 输出：[7,0,8]
+var l1 = new ListNode(2);
+l1.next = new ListNode(4);
+l1.next.next = new ListNode(3);
+
+var l2 = new ListNode(5);
+l2.next = new ListNode(6);
+l2.next.next = new ListNode(4);
+
+var l1 = new ListNode(5);
+
+var l2 = new ListNode(5);
+
+var result = addTwoNumbers(l1, l2);
+console.log(result);
