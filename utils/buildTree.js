4e335ebc445107bb541d04b0497ee0b0ad724081
mzvast <mzvast@gmail.com>
2021-05-10 01:14:47 +0800

    feat: add utils

---
+++ utils/buildTree.js
@@ -0,0 +1,25 @@
+import {TreeNode} from './TreeNode.js';
+
+// 从层序遍历构建二叉树
+
+export function buildTree(arr) {
+    const root = new TreeNode(arr[0]);
+    const q = [root]; // 处理过的节点
+    for (let i = 1; i < arr.length; ) {
+        const cur = q.shift();
+        if (arr[i]) {
+            cur.left = new TreeNode(arr[i]);
+            q.push(cur.left);
+        }
+        i++;
+
+        if (arr[i]) {
+            cur.right = new TreeNode(arr[i]);
+            q.push(cur.right);
+        }
+        i++;
+    }
+    return root;
+}
+// const test = buildTree([10, 5, -3, 3, 2, null, 11, 3, -2, null, 1]);
+// console.log(JSON.stringify(test));
