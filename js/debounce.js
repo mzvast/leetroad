bca19550c4543d035381a3ffee575f9cf75afc03
mzvast <mzvast@gmail.com>
2021-11-27 13:35:54 +0800

    debounce

---
+++ js/debounce.js
@@ -0,0 +1,17 @@
+function debounce(handler, delay) {
+    let timer = null;
+    return function debounced(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            handler.apply(this, args);
+        }, delay);
+    };
+}
+
+// test cases
+
+const f = debounce((v) => console.log(v), 100);
+f(1);
+f(2);
+f(3);
+
