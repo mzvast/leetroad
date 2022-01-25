ec1aecb9203f6936286225b99ed95475031e5e77
mzvast <mzvast@gmail.com>
2022-01-25 13:51:03 +0800

    add promise aplus tests

---
+++ js/promise/aplus/adaptor.js
@@ -0,0 +1,9 @@
+Promise.defer = Promise.deferred = function () {
+    let dfd = {};
+    dfd.promise = new Promise((resolve, reject) => {
+        dfd.resolve = resolve;
+        dfd.reject = reject;
+    });
+    return dfd;
+};
+module.exports = Promise;
