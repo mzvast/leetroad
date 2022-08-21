fdad5239b205ff9e65a642242c5dd8935545ab05
mzvast <mzvast@gmail.com>
2022-08-21 11:36:48 +0800

    add 153

---
+++ js/bbq.js
@@ -0,0 +1,33 @@
+// quicksort in place
+function quicksort(nums) {
+    const n = nums.length;
+    sort(0, n - 1);
+
+    function sort(l, r) {
+        if (l >= r) return;
+        const mid = partition(l, r);
+        sort(l, mid - 1);
+        sort(mid + 1, r);
+    }
+
+    function partition(l, r) {
+        let v = nums[l];
+        let i = l,
+            j = r + 1;
+        while (i < j) {
+            while (nums[++i] < v) if (i > r) break;
+            while (nums[--j] > v) if (j < l) break;
+            if (i >= j) break;
+            swap(i, j);
+        }
+        swap(l, j);
+        return j;
+    }
+
+    function swap(i, j) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+}
+const arr = [3, 2, 1, 4, 5, 6, 7, 8, 9, 10];
+quicksort(arr);
+console.log(arr);
