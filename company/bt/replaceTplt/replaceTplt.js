4495785c777c43c7cd85cf4fee61b4a71d76077d
mzvast <mzvast@gmail.com>
2024-04-12 11:46:40 +0800

    udpate replaceTmp

--- company/bt/replaceTplt/replaceTplt.js
+++ company/bt/replaceTplt/replaceTplt.js
@@ -3,6 +3,24 @@ function replace(str, data) {
         return data[key] ?? match;
     });
 }
+
+function replace2(str, data) {
+    let ans = '';
+
+    for (let i = 0; i < str.length; i++) {
+        // look forward
+        if (str[i] === '$' && str[i + 1] === '{') {
+            i += 2; // first key char
+            let key = '';
+            while (str[i] !== '}') key = key + str[i++];
+            ans = ans + data[key];
+        } else {
+            ans = ans + str[i];
+        }
+    }
+
+    return ans;
+}
 const ans = replace('${name} is ${age}, ${name} is happy', {
     name: 'John',
     age: 25,
